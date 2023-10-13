3aad77bf8ba163d7152f3a1bf4af3742e89260cd
zzaabbaa <bilonizkavik@gmail.com>
2023-10-13 22:28:09 +0300

    Add project files.

---
+++ Second lesson work tpm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second_lesson_work_tpm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
